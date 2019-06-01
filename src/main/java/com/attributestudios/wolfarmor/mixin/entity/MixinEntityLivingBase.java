cc7512407b82ebddec3b776213653495919ebec9
satyrnidae <maskreybe@centurionfox.net>
2019-05-31 23:29:43 -0400

    :beers: Naive in-progress mixin dev

--- src/main/java/com/attributestudios/wolfarmor/mixin/entity/MixinEntityLivingBase.java
+++ src/main/java/com/attributestudios/wolfarmor/mixin/entity/MixinEntityLivingBase.java
@@ -2,7 +2,11 @@ package com.attributestudios.wolfarmor.mixin.entity;
 
 import net.minecraft.entity.EntityLivingBase;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(EntityLivingBase.class)
 public abstract class MixinEntityLivingBase extends MixinEntity {
+
+    @Shadow
+    protected void damageArmor(float damage) {}
 }
