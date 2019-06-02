1691f9cfaabc219b72de7178b2d5da6e8d8a17eb
satyrnidae <maskreybe@centurionfox.net>
2019-06-01 23:47:59 -0400

    :boom: Moved wolf armor item class to api

--- src/main/java/dev/satyrn/wolfarmor/common/DataHelper.java
+++
@@ -1,9 +0,0 @@
-package dev.satyrn.wolfarmor.common;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.network.datasync.DataParameter;
-
-public abstract class DataHelper {
-
-    public static DataParameter<Boolean> CHESTED;
-}
