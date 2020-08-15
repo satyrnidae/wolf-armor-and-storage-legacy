b7d44e98a87a0e04f7971d566930bfd7f733fe2b
Isabel Maskrey <isabel@satyrn.dev>
2020-08-14 23:31:06 -0400

    🔥 Remove EntityWolfArmored

--- src/main/java/dev/satyrn/wolfarmor/common/event/RegistrationEventHandler.java
+++ src/main/java/dev/satyrn/wolfarmor/common/event/RegistrationEventHandler.java
@@ -3,7 +3,6 @@ package dev.satyrn.wolfarmor.common.event;
 import dev.satyrn.wolfarmor.api.item.WolfArmorMaterials;
 import dev.satyrn.wolfarmor.item.ItemWolfArmor;
 import dev.satyrn.wolfarmor.api.util.Resources;
-import dev.satyrn.wolfarmor.entity.passive.EntityWolfArmored;
 import dev.satyrn.wolfarmor.item.WolfArmorMaterial;
 import dev.satyrn.wolfarmor.item.crafting.RecipeWolfArmorDyes;
 import net.minecraft.item.Item;
@@ -28,11 +27,6 @@ public class RegistrationEventHandler {
                 new ItemWolfArmor(WolfArmorMaterials.GOLD).setTranslationKey("wolfarmor.goldWolfArmor").setRegistryName(Resources.ITEM_GOLD_WOLF_ARMOR),
                 new ItemWolfArmor(WolfArmorMaterials.DIAMOND).setTranslationKey("wolfarmor.diamondWolfArmor").setRegistryName(Resources.ITEM_DIAMOND_WOLF_ARMOR)
         );
-    }
-
-    @SubscribeEvent
-    public void registerEntities(RegistryEvent.Register<EntityEntry> event) {
-        event.getRegistry().register(new EntityEntry(EntityWolfArmored.class, "wolfarmor.wolf_armored").setRegistryName(Resources.ENTITY_WOLF_ARMORED_LOCATION));
     }
 
     @SubscribeEvent
