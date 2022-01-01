2c34238b7164213544c5078aaa10dd277554984e
Isabel Maskrey <isabel@satyrn.dev>
2022-01-01 03:16:36 -0500

    🔥Delete unit tests I was never going to finish these anyways.

--- src/test/groovy/dev/satyrn/wolfarmor/config/WolfArmorConfigSpec.groovy
+++
@@ -1,9 +0,0 @@
-package dev.satyrn.wolfarmor.config
-
-import spock.lang.Specification
-import spock.lang.Unroll
-
-@Unroll
-class WolfArmorConfigSpec extends Specification {
-    //TODO: Unit Tests
-}
