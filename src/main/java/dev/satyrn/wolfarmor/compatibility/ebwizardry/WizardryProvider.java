9d0de52cb96e4ae25b5603582d0b811f5a5fddc0
Isabel Maskrey <isabel@satyrn.dev>
2022-01-27 02:50:30 -0500

    ✨ Add more init events to compat Added more preinit, init, and postinit events to compatibility Renamed setup method to loadComplete

--- src/main/java/dev/satyrn/wolfarmor/compatibility/ebwizardry/WizardryProvider.java
+++ src/main/java/dev/satyrn/wolfarmor/compatibility/ebwizardry/WizardryProvider.java
@@ -6,6 +6,7 @@ import dev.satyrn.wolfarmor.api.compatibility.Provider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -18,7 +19,7 @@ public class WizardryProvider extends CompatibilityProvider {
      */
     @Override
     @SideOnly(Side.CLIENT)
-    public void setupClient() {
+    public void loadComplete_Client(FMLLoadCompleteEvent event) {
         @Nonnull final RenderManager manager = Minecraft.getMinecraft().getRenderManager();
         @Nonnull final String className = "electroblob.wizardry.entity.living.EntitySpiritWolf";
 
