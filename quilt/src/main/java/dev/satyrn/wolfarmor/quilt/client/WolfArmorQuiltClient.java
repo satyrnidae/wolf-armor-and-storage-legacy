d1f9594dcff542068e39a62256def253807ff433
Isabel Maskrey <isabel@satyrn.dev>
2025-02-22 21:42:05 -0500

    🐛 Fix quilt launch Still having an issue where it isn't pulling the correct mixin config.

--- quilt/src/main/java/dev/satyrn/wolfarmor/quilt/client/WolfArmorQuiltClient.java
+++ quilt/src/main/java/dev/satyrn/wolfarmor/quilt/client/WolfArmorQuiltClient.java
@@ -2,11 +2,13 @@ package dev.satyrn.wolfarmor.quilt.client;
 
 import dev.satyrn.wolfarmor.fabriclike.client.WolfArmorFabricLikeClient;
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
-public final class WolfArmorQuiltClient implements ModInitializer {
+@ClientOnly
+public final class WolfArmorQuiltClient implements ClientModInitializer {
     @Override
-    public void onInitialize(ModContainer mod) {
+    public void onInitializeClient(ModContainer mod) {
         // Run the Fabric-like setup.
         WolfArmorFabricLikeClient.init();
     }
